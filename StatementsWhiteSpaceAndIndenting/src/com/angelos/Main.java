4b4eb36c7880449d423c9ea0cfa3dcea70d70008
Angelos <toutziaris.angelos@outlook.com>
2019-09-08 01:52:15 +0300

    Add statements-whitespace-and-indenting file

---
+++ StatementsWhiteSpaceAndIndenting/src/com/angelos/Main.java
@@ -0,0 +1,21 @@
+package com.angelos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        anotherVariable--;
+        System.out.println("This is another one");
+    }
+}
