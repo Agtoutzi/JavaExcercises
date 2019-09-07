32eeb3ab8587d1f709ad13cf5c49f053b22c3919
Angelos <toutziaris.angelos@outlook.com>
2019-09-08 01:55:19 +0300

    Add switch file

---
+++ Switch/src/com/angelos/Main.java
@@ -0,0 +1,69 @@
+package com.angelos;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int value = 3;
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 6;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1, 2, 3, 4 or 5");
+                break;
+        }
+
+        char c = 'A';
+        switch (c) {
+            case 'A':
+                System.out.println("A was found");
+                break;
+            case 'B':
+                System.out.println("B was found");
+                break;
+            case 'C':
+                System.out.println("C was found");
+                break;
+            case 'D':
+                System.out.println("D was found");
+                break;
+            case 'E':
+                System.out.println("E was found");
+                break;
+            default:
+                System.out.println("Could not find A, B, C, D or E");
+                System.out.println("Not found");
+                break;
+        }
+
+        String month = "January";
+        switch (month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                break;
+        }
+    }
+}
