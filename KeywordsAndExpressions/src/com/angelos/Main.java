7c5519d844004e1ac6dfca6452ea74432f12e42f
Angelos <toutziaris.angelos@outlook.com>
2019-09-08 01:51:30 +0300

    Add keywords-and-expressions file

---
+++ KeywordsAndExpressions/src/com/angelos/Main.java
@@ -0,0 +1,14 @@
+package com.angelos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers
+        double miles = 60;
+        double kilometers = miles * 1.609344;
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("this is an expression");
+        }
+    }
+}
