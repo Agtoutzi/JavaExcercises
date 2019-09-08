8501431ced3d9ce70705c05a8fd1aa6a7d204e66
Angelos <toutziaris.angelos@outlook.com>
2019-09-08 17:34:10 +0300

    Add Parsing-values-from-string file

---
+++ ParsingValuesFromString/src/com/angelos/Main.java
@@ -0,0 +1,25 @@
+package com.angelos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String numberAsString = "2018";
+        System.out.println("numberAsString= " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+
+        String doubleAsString = "2018.125";
+
+        double doubleNumber = Double.parseDouble(doubleAsString);
+        System.out.println("double number = " + doubleNumber);
+
+        
+    }
+}
