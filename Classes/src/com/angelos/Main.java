83030219876414cb49423cc594bba30275efede9
Angelos <toutziaris.angelos@outlook.com>
2019-09-08 21:25:22 +0300

    Add Simple-Calculator file + use it from Classes Main

--- Classes/src/com/angelos/Main.java
+++ Classes/src/com/angelos/Main.java
@@ -7,5 +7,15 @@ public class Main {
         Car holden = new Car();
         porche.setModel("Carrera");
         System.out.println("Model is " + porche.getModel());
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
     }
 }
