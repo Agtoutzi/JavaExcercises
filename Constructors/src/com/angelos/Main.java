8af7f785ba1d14bae83b4724a1fc4ea370cf246a
Angelos <toutziaris.angelos@outlook.com>
2019-09-10 00:39:54 +0300

    Constructors: Add Wall file + use it from Main

--- Constructors/src/com/angelos/Main.java
+++ Constructors/src/com/angelos/Main.java
@@ -24,5 +24,13 @@ public class Main {
         System.out.println("VIP 2: " + vipCustomer2.getName() + " " + vipCustomer2.getMailAddress() + " " + vipCustomer2.getCreditLimit());
         VipCustomer vipCustomer3 = new VipCustomer("customer 3", 100, "customer3@mail.com");
         System.out.println("VIP 3: " + vipCustomer3.getName() + " " + vipCustomer3.getMailAddress() + " " + vipCustomer3.getCreditLimit());
+
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
