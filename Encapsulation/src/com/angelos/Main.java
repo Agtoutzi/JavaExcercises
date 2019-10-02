a0ecd6fe2e54c6c761376e79854d02de716b3338
Angelos <toutziaris.angelos@outlook.com>
2019-10-02 23:03:46 +0300

    Add Encapsulation example files

---
+++ Encapsulation/src/com/angelos/Main.java
@@ -0,0 +1,23 @@
+package com.angelos;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Player player = new Player();
+//        player.name = "Angelos";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("Angelos", 150, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+    }
+}
